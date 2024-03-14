28c9a00ca0d1588cf9e76e5b49827c5a57238f44
Marcin Świderski <marcin.swiderski@microsoft.wsei.edu.pl>
2024-03-14 10:22:35 +0100

    Added overrides for 'calculateStatistics' and 'CalculateStudentStatistics', cleaned up code on override 'GetLetterGrade'

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -26,30 +26,43 @@ namespace GradeBook.GradeBooks
             {
                 avgGrades.Add(student.AverageGrade);
             }
-
-            
             avgGrades = avgGrades.OrderByDescending(x => x).ToList();
             int top20students = (int)(avgGrades.Count*0.2);
 
-
             if ((avgGrades.Take(top20students).Contains(averageGrade) || ((avgGrades.Take(top20students).Last() < averageGrade))))
             { return 'A'; }
-            else if (avgGrades.Skip(top20students).Take(top20students).Contains(averageGrade) || (avgGrades.Skip(top20students).Take(top20students).Last() < averageGrade && avgGrades.Skip(top20students).Take(top20students).First() > averageGrade))
+            else if (avgGrades.Skip(top20students).Take(top20students).Last() <= averageGrade && avgGrades.Skip(top20students).Take(top20students).First() >= averageGrade)
                 return 'B';
-            else if (avgGrades.Skip(top20students * 2).Take(top20students).Contains(averageGrade) || (avgGrades.Skip(top20students * 2).Take(top20students).Last() < averageGrade && avgGrades.Skip(top20students * 2).Take(top20students).First() > averageGrade))
+            else if (avgGrades.Skip(top20students * 2).Take(top20students).Last() <= averageGrade && avgGrades.Skip(top20students * 2).Take(top20students).First() >= averageGrade)
                 return 'C';
-            else if (avgGrades.Skip(top20students * 3).Take(top20students).Contains(averageGrade) || (avgGrades.Skip(top20students * 3).Take(top20students).Last() < averageGrade && avgGrades.Skip(top20students * 3).Take(top20students).First() > averageGrade))
+            else if (avgGrades.Skip(top20students * 3).Take(top20students).Last() <= averageGrade && avgGrades.Skip(top20students * 3).Take(top20students).First() >= averageGrade)
                 return 'D';
             else
                 return 'F';
-
-
-
-
-
-
-
-            return 'X';
+        }
+        public override void CalculateStatistics()
+        {
+            if (Students.Count >= 5)
+            {
+                base.CalculateStatistics();
+            }
+            else 
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+                return;
+            }
+        }
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count >= 5)
+            {
+                base.CalculateStudentStatistics(name);
+            }
+            else
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+                return;
+            }
         }
     }
     
